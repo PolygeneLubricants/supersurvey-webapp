8af7f1dbe81b536a8a04305b901dc5882be0297b
Andreas R. L. Christiansen <Andreasrlc@gmail.com>
2014-05-07 15:30:25 +0200

    Fixed build error

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,11 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-html-smoosher');
 
 	//Default task(s).
-	grunt.registerTask('default', ['copy', 'html2js', 'concat', 'smoosher']);
+	grunt.registerTask('default', ['copy', 'html2js', 'concat']);
+	
+	// Create index (Time consuming. Takes about 30 seconds)
+	grunt.registerTask('purehtml', ['copy', 'html2js', 'concat', 'smoosher']);
+	
 	//other tasks
 	grunt.registerTask('jslint', ['jshint']);
 };
